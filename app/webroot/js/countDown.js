5ad4c13d57c1d3c1e59724de152afc8aad3d0057
base-p <okorieobumneke@gmail.com>
2018-01-25 09:06:52 +0100

    update timer

--- app/webroot/js/countDown.js
+++ app/webroot/js/countDown.js
@@ -41,7 +41,7 @@
     }
 
     var timers = document.querySelectorAll('.c-countDown');
-    var deadline = new Date('2018-01-29T00:00:00');
+    var deadline = new Date('2018-01-29T23:59:59');
 
     timers.forEach(function (timer) {
         initializeClock(timer, deadline);
